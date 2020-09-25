33a316cbed6bf086fa2613a77cb9b6e032e5c1b5
Damian Malysza <damian.malysza@gmail.com>
2020-09-25 03:02:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const LEFT_ARROW = 37 // use e.which!
 const RIGHT_ARROW = 39 // use e.which!
 const ROCKS = []
 const START = document.getElementById('start')
+const END=document.getElementById("end")
 
 var gameInterval = null
 
